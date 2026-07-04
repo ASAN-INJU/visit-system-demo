61f400cbf909cecf89a1c6b6c165cd3df1054d8b
ASAN-INJU <jjukssam755@gmail.com>
2026-07-04 21:45:19 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='visit-system-v3-demo-final-1';
-const FILES=['./','./index.html','./style.css','./houses.js','./app.js','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
